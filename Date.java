dba4eb4595cbfdd0965a8d82f2579be21330f70e
minhtran365411 <g00365411@gmit.ie>
2020-02-20 14:02:56 +0000

    t

--- Date.java
+++ Date.java
@@ -4,9 +4,9 @@
 public class Date {
     
     // Instance Variables
-    private int day;
-    private int month;
-    private int year;
+     int day;
+     int month;
+     int year;
 
     // Default Constructor (No Parameters)
     public Date(){
@@ -19,15 +19,22 @@ public class Date {
     // Parameterized constructor (3 Parameters)
     public Date(int newDay, int newMonth, int newYear){
         // Your code here.
-        
+        day = newDay;
+        month = newMonth;
+        year = newYear;
     }
     
     // Parameterized constructor (2 Parameters)
+    public Date(int bMonth, int bYear) {
     // Your code here.
-    
+        month = bMonth;
+        year = bYear;
+    }
     // Parameterized constructor (1 Parameter)
+    public Date(int today) {
     // Your code here.
-    
+        day = today;
+    }
     // A simple print method
     public void printDate(){
         // Print the date like this: day/month/year
--- Main.java
+++ Main.java
@@ -8,6 +8,14 @@ public class Main {
         System.out.println( dateObj1.day );
         
         // Create 3 more Date objects using all the paramiterised constructors
+        Date dateObj2 = new Date(20,02,2020);
+        System.out.println("Today is the "+dateObj2.day+ " of " + dateObj2.month + ", " + dateObj2.year  );
+
+        Date dateObj3 = new Date(01,2000);
+        System.out.println("Birthday is on "+dateObj3.month+" of "+dateObj3.year);
+
+        Date dateObj4 = new Date(20);
+        System.out.println("Today's date is "+dateObj4.day);
         // Call the printDate() method for each object you create.
     }
 }
